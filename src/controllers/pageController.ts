b39975cc4614ab7ca053415c0d15a969ea91bc9d
Leonardo Ronnau <leonardodanielronnau76@gmail.com>
2024-10-08 22:46:09 -0300

    menu integration

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,38 @@
 import { Request, Response } from "express";
 
 export const home = (req:Request, res:Response)=>{
-res.send('1')
+res.render('pages/page', {
+    banner : {
+        title : 'Todos os animais',
+        backGround: 'allanimals.jpg'
+
+    }
+})
 }
 export const dogs = (req:Request, res:Response)=>{
-    res.send('2')
+    res.render('pages/page', {
+        banner : {
+            title : 'Cachorros',
+            backGround: 'banner_dog.jpg'
+    
+        }
+    })
 }
 export const cats = (req:Request, res:Response)=>{
-    res.send('13')
+    res.render('pages/page', {
+        banner : {
+            title : ' Gatos ',
+            backGround: 'banner_cat.jpg'
+    
+        }
+    })
 }
 export const fishes = (req:Request, res:Response)=>{
-    res.send('41')
+    res.render('pages/page', {
+        banner : {
+            title : 'Peixes',
+            backGround: 'banner_fish.jpg'
+    
+        }
+    })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,34 @@
-import  express  from "express";
+import express from "express";
 import dotenv from 'dotenv';
 import mustache from "mustache-express";
-import path from 'path'
+import path from 'path';
 import mainRoutes from './routes/index';
 
+dotenv.config();  // Carrega variáveis de ambiente do arquivo .env
 
-dotenv.config()
+const server = express();
 
-
-const server = express()
-
-
+// Configurações do Mustache como engine de visualização
 server.set('view engine', 'mustache');
 server.set('views', path.join(__dirname, 'views'));
-server.engine('mustache', mustache())
-
-server.use(express.static(path.join(__dirname, '../public')))
+server.engine('mustache', mustache());
 
 
 
-//rotas do servidor
 
-server.use(mainRoutes)
-server.use((req,res)=> {
-    res.send('pagina não encontrada')
-})
+// Servindo arquivos estáticos
+server.use(express.static(path.join(__dirname, '../public')));
 
+// Rotas principais
+server.use(mainRoutes);
 
-server
+// Middleware para lidar com páginas não encontradas (404)
+server.use((req, res) => {
+    res.status(404).send('Página não encontrada');
+});
 
-server.listen(process.env.PORT)
+// Iniciando o servidor na porta definida no arquivo .env ou na porta 3000 como padrão
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
