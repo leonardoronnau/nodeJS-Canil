d9a1254f439ad27f55ac7028f32abe05ccb9002a
Leonardo Ronnau <leonardodanielronnau76@gmail.com>
2024-10-12 23:27:37 -0300

    Feature : creating function active menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
+import { createMenuObject } from "../helpers/createMenuObject";
 
 export const home = (req:Request, res:Response)=>{
 res.render('pages/page', {
+    menu: createMenuObject('all'),
     banner : {
         title : 'Todos os animais',
         backGround: 'allanimals.jpg'
@@ -11,6 +13,7 @@ res.render('pages/page', {
 }
 export const dogs = (req:Request, res:Response)=>{
     res.render('pages/page', {
+        menu: createMenuObject('dog'),
         banner : {
             title : 'Cachorros',
             backGround: 'banner_dog.jpg'
@@ -20,6 +23,7 @@ export const dogs = (req:Request, res:Response)=>{
 }
 export const cats = (req:Request, res:Response)=>{
     res.render('pages/page', {
+        menu: createMenuObject('cat'),
         banner : {
             title : ' Gatos ',
             backGround: 'banner_cat.jpg'
@@ -29,6 +33,7 @@ export const cats = (req:Request, res:Response)=>{
 }
 export const fishes = (req:Request, res:Response)=>{
     res.render('pages/page', {
+        menu: createMenuObject('fish'),
         banner : {
             title : 'Peixes',
             backGround: 'banner_fish.jpg'
